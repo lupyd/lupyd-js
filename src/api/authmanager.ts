d399edee6c89d76a356d3dad5dc7583944e2861e
ash-hashtag <hashtag438@gmail.com>
2025-04-15 11:58:24 +0530

    no bugs

--- src/api/authmanager.ts
+++ src/api/authmanager.ts
@@ -1,48 +1,47 @@
-import { Utils, base64UrlDecode } from "../bin/utils"
-import { refreshTokens } from "./authflow"
-
+import { base64UrlDecode } from "../bin/utils";
+import { refreshTokens } from "./authflow";
 
 interface LupydAccessToken {
-  exp: number,
-  iat: number,
-  uid: number,
-  uname: string,
+  exp: number;
+  iat: number;
+  uid: number;
+  uname: string;
 }
 
 export class AuthManager {
-  private accessToken: string
-  private refreshToken: string
-  private appId: string
-  private refreshTaskId: number = -1
+  private accessToken: string;
+  private refreshToken: string;
+  private appId: string;
+  private refreshTaskId: number = -1;
 
-
-  isLoggedIn(): bool {
-    return this.getDeserializedAccessToken().exp < (Date.now() / 1000)
+  isLoggedIn(): boolean {
+    return this.getDeserializedAccessToken().exp < Date.now() / 1000;
   }
-  
+
   getDeserializedAccessToken() {
-    const [_header, payload, _signature] = this.accessToken.split('.')
-    const token:LupydAccessToken = JSON.parse(base64UrlDecode(payload))
+    const [_header, payload, _signature] = this.accessToken.split(".");
+    const token: LupydAccessToken = JSON.parse(base64UrlDecode(payload));
     return token;
   }
 
-
   async refresh() {
     console.info("Refreshing Lupyd Tokens");
-    const tokens = await refreshTokens(this.refreshToken)
+    const tokens = await refreshTokens(this.refreshToken);
     if (tokens == null) {
       console.error("Failed to refresh tokens");
     } else {
-      clearTimeout(this.refreshTaskId)
+      clearTimeout(this.refreshTaskId);
       this.accessToken = tokens.accessToken;
       this.refreshToken = tokens.refreshToken;
 
-      const duration_in_ms = this.getDeserializedAccessToken().exp * 1000  - Date.now() - 3_00_000;
+      const duration_in_ms =
+        this.getDeserializedAccessToken().exp * 1000 - Date.now() - 3_00_000;
 
-      this.refreshTaskId = Number(setTimeout(() => {
-        this.refresh()
-      }, duration_in_ms))
+      this.refreshTaskId = Number(
+        setTimeout(() => {
+          this.refresh();
+        }, duration_in_ms),
+      );
     }
   }
-
 }
