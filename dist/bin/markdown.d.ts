6fd288367c8fc2b56cf7e6fe9f362a7014d05ed5
ash-hashtag <hashtag438@gmail.com>
2025-05-01 15:22:36 +0530

    redist

--- dist/bin/markdown.d.ts
+++
@@ -1,4 +0,0 @@
-import { PickedFileUrl } from "./utils";
-export declare const syncMarkdownToHTMLElement: (markdown: string, pickedFileUrls: PickedFileUrl[]) => HTMLDivElement;
-export declare const markdownToHtmlElement: (markdown: string, pickedFileUrls?: PickedFileUrl[]) => HTMLElement;
-export declare const ImagePreviewElement: (image: HTMLImageElement) => void;
